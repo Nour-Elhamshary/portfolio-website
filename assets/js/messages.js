6c5929313cc679cf6af876f50ea8e8d3dedd1a04
Nour-Elhamshary <nour.elhamshary@outlook.com>
2025-08-22 22:39:17 +0300

    Changed the model so that it's dynamic instead of static, which is better.

---
+++ assets/js/messages.js
@@ -0,0 +1,34 @@
+export const messages = {
+    aboutMe: 
+    `
+    <p>
+          Hello! Name's Nour Elhamshary, a software engineer with a variety of
+          skills at his toolset. Worked in both back-end and front-end
+          development, alongside working in various projects using variety of
+          programming languages like Java, C# and C++.
+          <br />
+          <br />
+          Fun fact! This website is made of pure HTML, CSS and JavaScript
+          (meaning no frameworks used like Angular or React) and that
+          <em>is</em> intentional! I am a firm believer of a simple solution
+          being the best solution, and a portfolio website does not need to be
+          über-fancy in order to send the point across.
+          <br />
+          <br />
+          Maybe except your name is Nour and you decided to use a fully-vanilla
+          Matrix effect just as a nice eye candy. :]
+          <br />
+          <br />
+          My full work experience can be found in the "My CV" button if you
+          want, and you can communicate with me through E-mail and LinkedIn in
+          the other side of the navigation bar! My GitHub is also available for
+          you to check on for some personal projects and/or projects I work as a
+          collaborator!
+          <br />
+          <br />
+          Whatever you may do around my website, I hope you have a wonderful
+          day! Do check on a button named Felix, you will be happy with that
+          decision. :]
+        </p>
+    `,
+}
--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 "use strict";
+import {messages} from "./assets/js/messages.js";
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
+const bodyEl = document.querySelector('body');
 const aboutMeBtn = document.querySelector(".about-me");
-const btnCloseModal = document.querySelector(".close-modal");
 
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+
+const modalEl = document.createElement('div');
+modalEl.classList.add('modal');
+
+const overlayEl = document.createElement('div');
+overlayEl.classList.add('overlay');
+
+const closeModalBtn = document.createElement('button');
+closeModalBtn.classList.add('close-modal');
+closeModalBtn.innerHTML = `<ion-icon name="close-outline"></ion-icon>`;
+
+
+
+
+const openModal = function(e) {
+  if (e?.preventDefault) e.preventDefault();
+
+  const className = e.currentTarget.classList.value;
+  
+  bodyEl.insertAdjacentElement('afterbegin', modalEl);
+  modalEl.insertAdjacentElement('afterbegin', closeModalBtn);
+  modalEl.insertAdjacentElement('afterend', overlayEl);
+
+  switch (className) {
+    case 'about-me':
+      modalEl.insertAdjacentHTML('beforeend', messages.aboutMe);
+      break;
+  }
+  
+}
+
+const closeModal = function() {
+  console.log("It works!");
+  bodyEl.removeChild(modalEl);
+  bodyEl.removeChild(overlayEl);
+  modalEl.innerHTML = '';
+}
 
 aboutMeBtn.addEventListener("click", openModal);
-
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+closeModalBtn.addEventListener("click", closeModal);
+overlayEl.addEventListener("click", closeModal);
